50ed02d643a717a15b3930e1738e36e26f85fd26
Kevin Mooney <kmooney517@gmail.com>
2015-10-05 14:54:51 -0400

    got the second clock done - and the colors changing with the time.

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-function getTime(){
+//Create overarching function for the first clock
+function getTimeOne(){
+
+  //Find newClock in the HTML doc at the firstClock id
+  var newClock = $('#firstClock')
 
   //Create new date object
-  var newClock = $('#firstClock')
   var time = new Date ();
 
   //Make variables for each time segment that we want
@@ -17,7 +20,53 @@ function getTime(){
   //Combine to form string needed to appear on screen
   var currentTime = convertedHours + ':' + convertedMinutes + ':' + convertedSeconds;
 
+  //Fill currentTime as text in newClock, on browser
   newClock.text(currentTime);
 };
 
-setInterval('getTime()', 1000);
+//Set getTimeOne to run every second
+setInterval('getTimeOne()', 1000);
+
+
+//-----------------------------Second Clock-----------------------------------//
+
+
+//Create overarching function for the second clock
+function getTimeTwo(){
+
+  //Find newClock in the HTML doc at the secondClock id
+  var newClock = $('#secondClock')
+
+  //Create new date object
+  var time = new Date ();
+
+  //Make variables for each time segment that we want
+  var hours = time.getHours();
+  var minutes = time.getMinutes();
+  var seconds = time.getSeconds();
+
+  //Convert time variables to show appropriately for clock
+  var convertedHours = (hours > 12) ? '0'+(hours - 12): hours;
+  var convertedMinutes = (minutes < 10) ? '0' + minutes : minutes;
+  var convertedSeconds = (seconds < 10) ? '0' + seconds : seconds;
+
+  //Combine to form string needed to appear on screen
+  var currentTime = convertedHours + convertedMinutes + convertedSeconds;
+
+  //Make sure that currentTime is being read as an integer
+  var timeAsHex = parseInt(currentTime);
+
+  //Answering the problem of the zero disappearing from the front of a number
+  var hexNumber = (timeAsHex > 99999) ? '#' + timeAsHex : '#0' + timeAsHex;
+
+  //Fill currentTime as text in newClock, on browser
+  newClock.text('#' + currentTime);
+
+  //Changing background color to match hex number
+  $('#secondClock').css('background-color', hexNumber);
+};
+
+  //Execute function every 1000 milliseconds
+setInterval('getTimeTwo()', 1000);
+
+
