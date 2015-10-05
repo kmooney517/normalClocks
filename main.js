6eb5a53d739473b9f07f02161a8bde45bc9c7545
Kevin Mooney <kmooney517@gmail.com>
2015-10-05 17:21:18 -0400

    finished hex conversion for hard mode

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ function getTimeOne(){
 };
 
 //Set getTimeOne to run every second
-setInterval('getTimeOne()', 1000);
+setInterval(getTimeOne, 1000);
 
 
 //-----------------------------Second Clock-----------------------------------//
@@ -67,6 +67,56 @@ function getTimeTwo(){
 };
 
   //Execute function every 1000 milliseconds
-setInterval('getTimeTwo()', 1000);
+setInterval(getTimeTwo, 1000);
+
+
+
+
+
+
+
+//-----------------------------Third Clock-----------------------------------//
+
+
+//Create overarching function for the second clock
+function getTimeThree(){
+
+  //Find newClock in the HTML doc at the secondClock id
+  var newClock = $('#thirdClock')
+
+  //Create new date object
+  var time = new Date ();
+
+  //Make variables for each time segment that we want
+  var hours = time.getHours();
+    var hoursHex = hours.toString(16);
+    var hour = ((hoursHex < 10)||(hoursHex === 'a')||(hoursHex ==='b')||(hoursHex ==='c')||(hoursHex ==='d')||(hoursHex==='e')||(hoursHex==='f')) ? '0' + hoursHex: hoursHex;
+
+
+  var minutes = time.getMinutes();
+    var minutesHex = minutes.toString(16);
+    var min = ((minutesHex < 10)||(minutesHex === 'a')||(minutesHex ==='b')||(minutesHex ==='c')||(minutesHex ==='d')||(minutesHex==='e')||(minutesHex==='f')) ? '0' + minutesHex: minutesHex;
+
+  // Take seconds, convert to base 16, if less than 10 or equal to a letter, add 0 in front
+  var seconds = time.getSeconds();
+    var secondsHex = seconds.toString(16);
+    var sec = ((secondsHex < 10)||(secondsHex === 'a')||(secondsHex ==='b')||(secondsHex ==='c')||(secondsHex ==='d')||(secondsHex==='e')||(secondsHex==='f')) ? '0' + secondsHex: secondsHex;
+
+ 
+
+ var hexTime = hour + min + sec;
+  //Fill currentTime as text in newClock, on browser
+  newClock.text(hexTime);
+
+};
+
+  //Execute function every 1000 milliseconds
+setInterval(getTimeThree, 1000);
+
+
+
+
+
+
 
 
